db8a43a63004e3d663ced8538f5a619ed9f3faca
pochaka <groznykh.timur@yandex.ru>
2019-02-15 10:34:44 +0300

    remove garbage

--- init.el
+++ init.el
@@ -102,17 +102,3 @@
 
 
 ;;; init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (neotree use-package rust-mode py-autopep8 powerline org-agenda-property material-theme magit flycheck elpy dockerfile-mode color-theme-modern autopair))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
